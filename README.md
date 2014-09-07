0a82324da70b6f048bc861000ebb3bf6ce120439
Tudor Dragan <tudorgk@gmail.com>
2014-09-07 17:08:45 +0200

    modifying README

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-AP-Assignment0
-==============
+AP-Assignment0 - Look at Those Curves
+=====================================
 
-Advanced Programming - Look at those curves
+SYNOPSIS
+--------
+
+The objective of this assignment is to gain hands-on programming experience with Haskell.
+
+The goal is to implement a library, Curves, for working with piecewise linear curves in the plane (based on an idea by Hansen and Rischel, who in turn got the idea from Maarten M. Fokkinga).
--- assignment0.hs
+++ assignment0.hs
@@ -7,10 +7,13 @@ import System.IO()
 data Point = Point { x :: Double, y :: Double} deriving (Show)
 type Curve = [Point]
 
-
+--Point constructor
 point :: (Double, Double) -> Point
 point a = Point { x = (fst a) , y = (snd a)}
 
+
+--Comparing points. Didn't know how to remove the warnings. If you can tell me how
+--to do this comparison without those warnings, I would be very grateful :)
 comparePoints :: Point -> Point -> Bool
 comparePoints a b = (truncate ((x a) * 100)) - (truncate ((x b)* 100)) == 0 && (truncate ((y a) * 100)) - (truncate ((y b)* 100)) == 0
 
@@ -18,13 +21,16 @@ instance Eq Point where
 	a == b = comparePoints a b
 	a /= b = not (comparePoints a b)
 
+--Curve constructor
 curve :: Point -> [Point] -> Curve
 curve aPoint [] = [aPoint]
 curve aPoint listPoints =  aPoint:listPoints
 
+--List concat
 connect :: Curve -> Curve -> Curve
 connect c1 c2 = c1 ++ c2
 
+--Curve rotation
 rotatePoint :: Double -> Point -> Point
 rotatePoint theta aPoint
 	= Point { x = (x aPoint) * c + (y aPoint) * s , y = (y aPoint) * c - (x aPoint) * s}
@@ -34,6 +40,7 @@ rotate :: Curve -> Double -> Curve
 rotate points theta 
 	= map (rotatePoint theta) points
 
+--Curve translation
 translatePoint :: Point -> Point -> Point
 translatePoint cPoint pPoint = Point { x = (x cPoint) + (x pPoint), y = (y cPoint) + (y pPoint)}
 
@@ -43,6 +50,7 @@ translate (firstPoint:points) pPoint
 
 data Axis = Vertical | Horizontal deriving (Eq, Show)
 
+--Curve reflection
 reflectPoint :: Axis -> Double -> Point ->  Point
 reflectPoint a aValue cPoint = 
 	if a == Horizontal
@@ -55,11 +63,11 @@ reflect points axis axisValue
 	= map (reflectPoint axis axisValue) points
 
 
+--Bounding box
 findMinMaxValues :: (Point, Point) -> Point -> (Point, Point)
 findMinMaxValues (p1, p2) comparePoint = (Point {x = (min (x p1) (x comparePoint)) , y = (min (y p1) (y comparePoint))},
 										  Point {x = (max (x p2) (x comparePoint)) , y = (max (y p2) (y comparePoint))})
 
---TODO: Fix bounding box
 bbox :: Curve -> (Point, Point)
 bbox [] = error "no items in the list"
 bbox (aPoint:[]) = (aPoint, aPoint)
@@ -69,6 +77,7 @@ bbox points =
 				Point {x = (max (x (head points)) (x (head (tail points)))) , y = (max (y (head points)) (y (head (tail points))))})
 		in foldl findMinMaxValues (minPoint, maxPoint) (tail (tail points))
 
+--Width & Height
 width :: Curve -> Double
 width points = abs((x (fst a))) + (x (snd a))
 	where a = bbox points
@@ -77,7 +86,7 @@ height :: Curve -> Double
 height points = abs((y (fst a))) + (y (snd a))
 	where a = bbox points
 
-
+--Printing functions
 toList :: Curve -> [Point]
 toList [] = []
 toList (firstPoint:points) = firstPoint : (toList points)
@@ -99,10 +108,14 @@ toSVG points =
 	 ++ (show (ceiling (height points))) ++ "px\" version=\"1.1\"><g>" 
 	 ++ printPoints points
 
+
+--Exporting to file
 toFile :: Curve -> FilePath -> IO ()
 toFile points filePath=
 	writeFile filePath (toSVG points)
 
+
+--Testing hilbert
 hilbert :: Curve -> Curve
 hilbert c = c0 `connect` c1 `connect` c2 `connect` c3
    where  w = width c
@@ -117,8 +130,3 @@ hilbert c = c0 `connect` c1 `connect` c2 `connect` c3
           c3 = ch `rotate` 90 `translate` (point (0, h+p))
 
 
---TODO: Sanity Checks 
---let p2 = point (4, 2)
---let c1 = [point (2, 1),point (4, 5), point (3,3),point (2,5)]
---c2 = [point (3.62,2.5), point (-2.5, -2.1), point (2.1,8.2), point (-4, -1.21), point (5, -6), point(-6,-5)]
-c12 = [point (4,3), point (-2,4), point (-3,-3), point (4,2), point (2,-3), point (4,6)]
